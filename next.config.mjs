bbe603472353a475b07c702ec33f24a4461aea14
personal <rickykuo925@gmail.com>
2024-03-26 08:05:36 +0000

    dev

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,19 @@ import path from "path";
 
 const nextConfig = {
   webpack: (config, { isServer, dev }) => {
+    console.log("dev", dev);
     // Add the module rule to remove code blocks
-    config.module.rules.push({
-      test: /\.js$/, // files we want to process
-      exclude: /(node_modules|bower_components|\.spec\.js)/, // files we want to exclude
-      use: [
-        {
-          loader: "webpack-remove-code-blocks", // use the loader
-        },
-      ],
-    });
+    if (!dev) {
+      config.module.rules.push({
+        test: /\.(js|jsx|ts|tsx)$/, // files we want to process
+        exclude: /(node_modules|bower_components|\.spec\.js)/, // files we want to exclude
+        use: [
+          {
+            loader: "webpack-remove-code-blocks", // use the loader
+          },
+        ],
+      });
+    }
 
     return config;
   },
